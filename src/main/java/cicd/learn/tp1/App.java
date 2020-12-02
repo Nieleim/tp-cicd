b94084e5bd08cae07fac58688abe3731288f3cc2
Wacrenier <wacrenieremilien@gmail.com>
2020-12-02 09:32:50 +0100

    commit feature jenkins 2

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("002");
         System.out.println("V1.2");
         System.out.println("ftjenkins1");
+        System.out.println("ftjenkins2");
     }
 }
